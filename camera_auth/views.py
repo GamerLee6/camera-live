5f497afb0445c28d0a22887e5c6a624deb4e0d3f
GamerLee6 <756092470@qq.com>
2022-03-19 16:09:02 +0800

    v1.0

---
+++ camera_auth/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse,JsonResponse, request
+import json
+from mymodule.get_status import *
+from django.views.decorators.csrf import csrf_exempt
+ 
+def hello(request):
+    return HttpResponse("Hello world ! ")
+
+def front(request):
+    return render(request, 'weblive.html')  
+
+def template(request):
+    return render(request, 'bili.html')  
+
+@csrf_exempt
+def getauthresult(request):
+    print('reach')
+    if request.method == 'POST':
+        req = json.loads(request.body)
+        try:
+            ret = run('10.4.122.2','8081')
+            print(ret)
+        except:
+                returnJsonResponse({'result': 200, 'msg': 'unknow'})
+        if ret == 1:
+            return JsonResponse({'result': 200, 'msg': 'safe'})
+        else:
+            return JsonResponse({'result': 200, 'msg': 'unsafe'})
---
+++ static/js/myfun.js
@@ -0,0 +1,126 @@
+var interval;
+var flvPlayer;
+var h = 480;
+var w = 640;
+var player = null;
+
+function ajaxResponse(xhr, successFunction, falseFunction) {
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if ((xhr.status >= 200 && xhr.status < 300) || xhr.status === 304) {
+                successFunction();
+            } else {
+                // alert("失败" + xhr.status);
+                falseFunction();
+            }
+        }
+    }
+};
+
+
+function start_play() {
+    player = new ZLMRTCClient.Endpoint(
+        {
+            element: document.getElementById('video'),// video 标签
+            debug: true,// 是否打印日志
+            zlmsdpUrl: 'http://10.4.122.2/index/api/webrtc?app=live&stream=video&type=play',//流地址
+            simulcast: false,
+            useCamera: false,
+            audioEnable: false,
+            videoEnable: true,
+            recvOnly: true,
+            resolution: { w: w, h: h }
+        }
+    );
+
+    player.on(ZLMRTCClient.Events.WEBRTC_ICE_CANDIDATE_ERROR, function (e) {// ICE 协商出错
+        console.log('ICE 协商出错')
+    });
+
+    player.on(ZLMRTCClient.Events.WEBRTC_ON_REMOTE_STREAMS, function (e) {//获取到了远端流，可以播放
+        console.log('播放成功', e.streams)
+    });
+
+    player.on(ZLMRTCClient.Events.WEBRTC_OFFER_ANWSER_EXCHANGE_FAILED, function (e) {// offer anwser 交换失败
+        console.log('offer anwser 交换失败', e)
+        stop();
+    });
+
+    player.on(ZLMRTCClient.Events.WEBRTC_ON_LOCAL_STREAM, function (s) {// 获取到了本地流
+
+        // document.getElementById('selfVideo').srcObject = s;
+        // document.getElementById('selfVideo').muted = true;
+
+        //console.log('offer anwser 交换失败',e)
+    });
+
+    player.on(ZLMRTCClient.Events.CAPTURE_STREAM_FAILED, function (s) {// 获取本地流失败
+
+        console.log('获取本地流失败')
+    });
+
+    player.on(ZLMRTCClient.Events.WEBRTC_ON_CONNECTION_STATE_CHANGE, function (state) {// RTC 状态变化 ,详情参考 https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/connectionState
+        console.log('当前状态==>', state)
+    });
+}
+
+let startVideoBt = document.getElementById('startVideoBt');
+startVideoBt.onclick = function start() {
+    stop();
+    start_play()
+}
+
+let stopVideoBt = document.getElementById('stopVideoBt');
+stopVideoBt.onclick = function stop() {
+    if (player) {
+        player.close();
+        player = null;
+    }
+}
+
+
+function timelyAuth() {
+    let staForm = document.getElementById('status');
+    let xhrRegister = new XMLHttpRequest();
+
+    ajaxResponse(xhrRegister,
+        function () {
+            let response = JSON.parse(xhrRegister.responseText);
+            console.log(response.msg);
+            if (response.msg === 'safe') {
+                console.log('safe');
+                staForm.innerHTML = '可信';
+                staForm.style.color = 'black';
+            } else {
+                console.log('unsafe');
+                staForm.innerHTML = '不可信';
+                staForm.style.color = 'red';
+            }
+
+        }, function () {
+            // let respones = JSON.parse(xhrRegister.responseText);
+
+        });
+
+    xhrRegister.open('POST', '../getstatus/');
+    xhrRegister.setRequestHeader('Content-type', 'application/x-www-form-urlencoded;charset=utf-8');
+    xhrRegister.send(JSON.stringify("getsta"));
+};
+
+
+
+let subStartBt = document.getElementById('startAuthBt');
+subStartBt.onclick = function () {
+    console.log("start checking");
+    interval = setInterval(timelyAuth, 30);
+};
+
+
+let endAuthBt = document.getElementById('endAuthBt');
+endAuthBt.onclick = function () {
+    console.log("stop checking");
+    clearInterval(interval);
+};
+
+
+
